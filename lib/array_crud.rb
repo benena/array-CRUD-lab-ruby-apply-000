9cb2cd463f783b348e9d0a2ade4c3099012672ef
Benjamin Ko <benjamintko@gmail.com>
2020-02-26 23:59:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  [1, 1, "1", 2].pop
+  [1, 1, "1", "arrays!"].pop
 end
 
 def remove_element_from_start_of_array(array)
